32779b17d6114e7261f0a665deabbdbe35ab9831
Hansol Kwon <home@Hansolui-iMac.local>
2019-05-05 20:38:15 +0900

    first commit

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Hansol Kwon on 01/05/2019.
+//  Copyright © 2019 Hansol Kwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
