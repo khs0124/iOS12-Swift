a38d7ef74dddf4670812807baf12245517bdd9f8
Hansol Kwon <home@Hansolui-iMac.local>
2019-06-06 21:53:41 +0900

    Update Quizzler App

---
+++ Quizzler/Quizzler/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Hansol Kwon on 27/05/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let questionText : String
+    let answer : Bool
+    
+    init(text: String, correctAnswer : Bool) {
+        questionText = text
+        answer = correctAnswer
+    }
+}
